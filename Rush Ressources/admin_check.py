1ab9076faf4029a3b93c73e7625521b6f85b19c4
DARKNOSY <109553205+DARKNOSY@users.noreply.github.com>
2023-06-22 22:16:56 +0100

    Create admin_check.py

---
+++ Rush Ressources/admin_check.py
@@ -0,0 +1,12 @@
+#not from me blud
+
+import ctypes
+
+def is_admin():
+    return ctypes.windll.shell32.IsUserAnAdmin() != 0
+
+def admin_check():
+    print("Checking if admin...")
+    print("```" + str(is_admin()) + "```")
+
+admin_check()
